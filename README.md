48d6cae1f0f4d2b7e3cd56f30ea1ce0e5004a8c1
rroyGit <rupam95@yahoo.com>
2018-12-28 13:56:01 -0500

    slight change to css; show only temp number; dry ajax code

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
   - Express
 ##### Socket code
   - C
+
+
+
+##### Web service run
+  nodejs index.js <PORT>
+  nodejs index.js 3001
+
+##### Web server run
+  nodejs index.js <PORT> <HOST:PORT>
+  nodejs index.js 3002 http://localhost:3001
--- web_server_laptop/dynamicData.js
+++ web_server_laptop/dynamicData.js
@@ -2,29 +2,43 @@ var sensor1Container = document.getElementById("sensor1");
 var sensor2Container = document.getElementById("sensor2");
 var counter = 0;
 
+var request;
+var request2;
 
-function getTemps () {
-    console.log("Getting update...");
+
+function init () {
+    
+    request = new XMLHttpRequest();
+    request2 = new XMLHttpRequest();
+
+    const getNumber = (data) => {
+        const regex = /([-| ]\d+)/;
+        const value = regex.exec(data);
+        return value[1];
+    }
+
+    request.onload = function () {
+        sensor1Container.innerHTML = getNumber(request.responseText);
+    }
+
+    request2.onload = function () {
+
+        sensor2Container.innerHTML = getNumber(request2.responseText);
+    }
+}
+
+
+function sendRequests () {
     counter++;
-
-    if (counter == 20) clearInterval(myInterval);
-
-    var request = new XMLHttpRequest();
-    var request2 = new XMLHttpRequest();
 
     request.open("GET", "http://localhost:3002/temp/1");
     request2.open("GET", "http://localhost:3002/temp/2");
-
-    request.onload = function() {
-        sensor1Container.innerHTML = request.responseText;
-    }
-
-    request2.onload = function() {
-        sensor2Container.innerHTML = request2.responseText;
-    }
-
     request.send();
     request2.send();
+
+    if (counter === 20) clearInterval(myInterval);
 }
 
-var myInterval = setInterval(getTemps, 2000);
+
+init();
+var myInterval = setInterval(sendRequests, 2000);
