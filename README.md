ae0022cfe6fee8136dac207a89aa4645a2866fb0
rroyGit <rupam95@yahoo.com>
2018-12-30 22:29:52 -0500

    add event handlers for buttons; add error handling and css/html changes

--- README.md
+++ README.md
@@ -16,3 +16,13 @@
 ##### Web server run
   nodejs index.js \<PORT\> \<HOST:PORT\>  
   nodejs index.js 3002 http://localhost:3001
+
+##### running on devices connect to the same network
+  (dynamicData.js) change PATH variable to the device's network IP that the server is running (e.g. 192.168.1.23)
+  See below how to retrive the ip
+##### Windows IPv4 address
+  Go to Windows Settings --> Network & Internet --> Wi-Fi --> Hardware Properties --> IPv4 address
+##### Linux IPv4 address
+  Google it...
+##### Mac IPv4 address
+   Google it...
--- web_server_laptop/dynamicData.js
+++ web_server_laptop/dynamicData.js
@@ -1,45 +1,90 @@
-var sensor1Container = document.getElementById("sensor1");
-var sensor2Container = document.getElementById("sensor2");
-var counter = 0;
+var sensor1Container = document.getElementById("tempSensor1");
+var sensor2Container = document.getElementById("tempSensor2");
+
+var startButton = document.getElementById("startButton");
+var stopButton = document.getElementById("stopButton");
+
+var state = "STOP";
+var myInterval;
+
+const PATH = "192.168.1.11";
+
+startButton.addEventListener("mouseup", function(){
+    if (request !== undefined && request2 !== undefined && state !== "START") {
+        stopButton.style.border = null;
+
+        state = "START"
+        this.classList.remove('mouse-down');
+        this.style.borderColor = "#ffffff";
+
+        sendRequests();
+        myInterval = setInterval(sendRequests, 2000);
+    } else {
+        // report error
+    }
+});
+
+stopButton.addEventListener("mouseup", function(){
+    if (request !== undefined && request2 !== undefined && state !== "STOP" ) {
+        startButton.style.border = null;
+
+        state = "STOP"
+        this.classList.remove('mouse-down');
+        this.style.borderColor = "#ffffff";
+
+        clearInterval(myInterval);
+    } else {
+        // report error
+    }
+});
 
 var request;
 var request2;
 
-
 function init () {
-    
     request = new XMLHttpRequest();
     request2 = new XMLHttpRequest();
+    
 
     const getNumber = (data) => {
-        const regex = /([-| ]\d+)/;
-        const value = regex.exec(data);
-        return value[1];
+        const regex = /(-*\d+\.?\d*)/;
+        let value = regex.exec(data);
+
+        if (value === null) return null;
+        
+        value = value[1];
+        if (value.slice(-1) === '.') value + '0';
+        return value;
     }
 
     request.onload = function () {
         sensor1Container.style.color = "#ff0026";
-        sensor1Container.innerHTML = getNumber(request.responseText);
+        let value;
+        if ((value = getNumber(request.responseText)) != null) {
+            sensor1Container.innerHTML = value;
+        } else {
+            sensor1Container.innerHTML = "ERROR";
+        }
     }
 
     request2.onload = function () {
         sensor2Container.style.color = "#ff0026";
-        sensor2Container.innerHTML = getNumber(request2.responseText);
+        let value;
+        if ((value = getNumber(request2.responseText)) != null) {
+            sensor2Container.innerHTML = value;
+        } else {
+            sensor2Container.innerHTML = "ERROR";
+        }
     }
 }
 
-
 function sendRequests () {
-    counter++;
+    request.open("GET", `http://${PATH}:3002/temp/1`);
+    request2.open("GET", `http://${PATH}:3002/temp/2`);
 
-    request.open("GET", "http://localhost:3002/temp/1");
-    request2.open("GET", "http://localhost:3002/temp/2");
     request.send();
     request2.send();
-
-    if (counter === 20) clearInterval(myInterval);
 }
 
-
 init();
-var myInterval = setInterval(sendRequests, 2000);
+
